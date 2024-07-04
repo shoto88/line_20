35c0e6f529cde11067655567ff50773a87641387
Otone Hamada <otonehamada@OtonenoMacBook-Pro.local>
2024-07-04 19:27:24 +0900

    first commit

--- src/components/display.tsx
+++ src/components/display.tsx
@@ -185,11 +185,11 @@ const WaitingRoomDisplay = () => {
         {status === 'pending' ? (
           <p className="text-3xl">読み込み中...</p>
         ) : (
-          <div className="w-full max-w-8xl bg-blue-100 rounded-lg p-5 pt-0 shadow-lg">
-            <h2 className="text-center font-bold text-6xl mb-4">
+          <div className="w-full max-w-8xl bg-blue-100 rounded-lg p-5 pt-2 shadow-lg">
+            <h2 className="font-bold text-6xl mb-4">
               待ち番号一覧
             </h2>
-            <div className="grid grid-cols-6 gap-3">
+            <div className="grid grid-cols-6 gap-2">
               {data?.queueStatus.filter(item => item.status === 0).slice(0, 20).map(item => (
                 <div key={item.number} className="w-44 h-44 flex items-center justify-center bg-yellow-100 rounded-lg m-0 shadow">
                   <span className="text-8xl font-bold">
